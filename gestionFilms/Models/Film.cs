52f8c5fa27746b14ce69c474b9d2aabd51a2f89b
Nora H. <nora.hashani01@gmail.com>
2026-04-07 10:06:16 +0200

    Ajout d'annotation dans film.cs

--- gestionFilms/Models/Film.cs
+++ gestionFilms/Models/Film.cs
@@ -1,10 +1,16 @@
-﻿namespace gestionFilms.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gestionFilms.Models
 {
     public class Film
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Titre { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Le genre est obligatoire")]
         public string Genre { get; set; } = string.Empty;
+        [Range(1900, 2030, ErrorMessage = "L'année doit être comprise entre 1900 et 2030")]
         public int Annee { get; set; }
     }
 }
